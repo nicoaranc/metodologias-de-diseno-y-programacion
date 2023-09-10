190a8f00ece9996ae8390560d9cbded009e8fe1e
nicocaranc <nicolas.arancibia.aguirre@gmail.com>
2023-09-10 13:20:21 -0300

    Se agrega el Proyecto

--- src/main/scala/model/paneles/neutral.scala
+++ src/main/scala/model/paneles/neutral.scala
@@ -1,8 +1,13 @@
 package cl.uchile.dcc.citric
 package paneles
+package model
+package traits
 
 import scala.collection.mutable.ArrayBuffer
 
-class neutral extends Panel{
+class neutral extends panel{
 
+  val characters: ArrayBuffer[PlayerCharacter]
+
+  var nextPanels
 }
